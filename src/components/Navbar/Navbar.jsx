949ae6608c3f20b7a869bf2db31d07be6aa2bbe9
Emirlan Dogdurbaev <jigit38@gmail.com>
2024-05-17 22:40:16 +0600

    added info for about us

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,32 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import classes from "./Navbar.module.scss";
 
 const Navbar = () => {
-    return (
-        <header className={classes.Navbar}>
-            <nav className={classes.nav}>
-                <div>
-                    <h3>
-                        <Link to={"/"}>Fabulous programmers</Link>
-                    </h3>
-                </div>
-                <ul>
-                    <li>
-                        <Link to={"/"}>Home</Link>{" "}
-                    </li>
-                    <li>
-                        <Link to={"/about"}>About us</Link>
-                    </li>
-                    <li>
-                        <Link to={"/projects"}>Projects</Link>
-                    </li>
-                    <li>
-                        <Link to={"/achivments"}>Achivments</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header className={classes.Navbar}>
+      <nav className={classes.nav}>
+        <div>
+          <h3>
+            <Link to={"/"}>Fabulous programmers</Link>
+          </h3>
+        </div>
+        <ul>
+          <li>
+            <Link to={"/"}>Home</Link>{" "}
+          </li>
+          <li>
+            <Link to={"/about"}>About us</Link>
+          </li>
+          <li>
+            <Link to={"/projects"}>Projects</Link>
+          </li>
+          <li>
+            <Link to={"/achivments"}>Achivments</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
